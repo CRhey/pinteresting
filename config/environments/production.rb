6beaee01cd3f09c1fcbdf421700a8ccc44aa18b0
Siarhei <s.samuseu@gmail.com>
2014-05-05 23:37:14 -0700

    Implemented device for users

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,4 +80,7 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  # Required for heroku - needs to be set
+  config.action_mailer.default_url_options = { host: 'omfg-pinteresting.com' }
 end
